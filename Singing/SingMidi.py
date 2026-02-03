fd2e72f98f08a62e83043a0296f258d9a6978545
Alan <bidnessmail15@gmail.com>
2026-02-02 17:21:58 -0800

    Volume control! 0.3 sounds best

--- Singing/SingMidi.py
+++ Singing/SingMidi.py
@@ -8,15 +8,14 @@ BUZZER = PWMOutputDevice(18)
 def midi_to_freq(note):
     return 440.0 * (2 ** ((note - 69) / 12))
 
-def play_midi(filename):
+def play_midi(filename, volume=0.5):
     mid = MidiFile(filename)
 
-    # Mido handles tempo and timing internally when using .play()
     for msg in mid.play():
         if msg.type == 'note_on' and msg.velocity > 0:
             freq = midi_to_freq(msg.note)
             BUZZER.frequency = freq
-            BUZZER.value = 0.5
+            BUZZER.value = volume
         elif msg.type in ('note_off', 'note_on') and msg.velocity == 0:
             BUZZER.value = 0
 
@@ -24,7 +23,11 @@ def play_midi(filename):
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Usage: python play_midi.py <file.mid>")
+        print("Usage: python play_midi.py <file.mid> [volume]")
         sys.exit(1)
 
-    play_midi(sys.argv[1])
+    filename = sys.argv[1]
+    volume = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+
+    play_midi(filename, volume)
+
